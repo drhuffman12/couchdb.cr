0a9ca7e9b4cd749e4c2742411c12db2cae401a2c
Daniel Huffman <drhuffman12@gmail.com>
2021-09-06 03:02:13 -0400

    drhuffman12/upgrade_to_crystal_1.1.1 avoid "Missing ENV key" errors; add more db conn env var's

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,7 +3,12 @@ require "spec"
 require "../src/couchdb"
 
 def new_client : CouchDB::Client
-  couchdb_url = ENV["TEST_DB"]? || "http://admin:password@localhost:5984"
+  un = ENV.keys.includes?("TEST_DB_UN") ? ENV["TEST_DB_UN"] : "admin"
+  pw = ENV.keys.includes?("TEST_DB_PW") ? ENV["TEST_DB_PW"] : "password"
+  ip = ENV.keys.includes?("TEST_DB_IP") ? ENV["TEST_DB_IP"] : "localhost"
+  port = ENV.keys.includes?("TEST_DB_PORT") ? ENV["TEST_DB_PORT"] : "5984"
+  couchdb_url = ENV.keys.includes?("TEST_DB") ? ENV["TEST_DB"] : "http://#{un}:#{pw}@#{ip}:#{port}"
+
   CouchDB::Client.new couchdb_url
 end
 
